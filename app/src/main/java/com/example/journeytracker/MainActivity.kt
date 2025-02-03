4fb4c6dec5fe8e6265e496a1139591474528ff9c
Yash Rajput <yashrajputyr10@gmail.com>
2025-02-04 00:08:25 +0530

    Add distance toggle and journey progress tracking
    Implemented distance conversion between kilometers and miles using a toggle button.
    Added progress tracking for journey stops with a ProgressBar update.
    Included nextStop() function to increase progress when reaching a new stop.
    Updated UI dynamically by modifying TextView content based on user actions.
    Ensured proper state management with isKm, progress, currentStop, and totalStops variables.

--- app/src/main/java/com/example/journeytracker/MainActivity.kt
+++ app/src/main/java/com/example/journeytracker/MainActivity.kt
@@ -1,20 +1,57 @@
 package com.example.journeytracker
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private var distanceInKm = 500
+    private var isKm = true
+    private var progress = 30
+    private var totalStops = 5
+    private var currentStop = 1
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val tvDistance = findViewById<TextView>(R.id.tvDistance)
+        val btnToggleDistance = findViewById<Button>(R.id.btnToggleDistance)
+        val btnNextStop = findViewById<Button>(R.id.btnNextStop)
+        val progressBar = findViewById<ProgressBar>(R.id.progressBar)
+
+        btnToggleDistance.setOnClickListener{
+            if(this.isKm){
+                val distanceInMiles = distanceInKm*0.621
+                tvDistance.text = "Distance: %.2f miles".format(distanceInMiles)
+                btnToggleDistance.text = "Convert to KMs"
+            }
+            else{
+                tvDistance.text = "Distance: $distanceInKm km"
+                btnToggleDistance.text = "Convert to Miles"
+            }
+            isKm = !isKm
         }
+
+        btnNextStop.setOnClickListener{
+            fun nextStop(){
+                if(currentStop < totalStops){
+                    currentStop++;
+                    progress += (100/ totalStops)
+                    progressBar.progress = progress
+                }
+            }
+
+        }
+
+        }
+
     }
-}
